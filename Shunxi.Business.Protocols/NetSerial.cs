edb6b227eee44fb5599db5c21ad0d74f697e0097
zhanglei <simple0812@sina.cn>
2017-09-22 10:19:33 +0800

    feature:使用net转发串口通信

--- Shunxi.Business.Protocols/NetSerial.cs
+++ Shunxi.Business.Protocols/NetSerial.cs
@@ -2,47 +2,48 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.Networking;
-using Windows.Networking.Sockets;
-using Windows.Storage.Streams;
 using Shunxi.Business.Enums;
 using Shunxi.Business.Protocols.Helper;
+using Shunxi.Common.Log;
 using Shunxi.Infrastructure.Common.Extension;
-using Shunxi.Infrastructure.Common.Log;
-using Shunxi.Infrastructure.Common.Utility;
+using SuperSocket.ProtoBase;
 
 namespace Shunxi.Business.Protocols
 {
     public class NetSerial: ISerial
     {
-        private readonly StreamSocket client = null;
+        private readonly SocketClient client = null;
         private bool isConnect => null != client;
         private static object _locker = new object();
-        private const string PORT = "3084";
-        private const string ADDR = "100.0.0.54";
+        private const int PORT = 3084;
+        private const string ADDR = "10.0.0.44";
         public SerialPortStatus Status { get; set; }
         CancellationTokenSource _readCancellationTokenSource;
         public event Action<byte[]> ReceiveHandler;
-        private DataReader reader;
 
         public NetSerial()
         {
-            client = new StreamSocket();
+            client = new SocketClient();
+
             Status = SerialPortStatus.Initialled;
         }
 
         public async Task Open(SerialEnum serialType)
         {
+            await Task.Yield();
             if (Status == SerialPortStatus.Initialled)
             {
                 try
                 {
-                    var serverHost = new HostName(ADDR);
                     Status = SerialPortStatus.Opening;
-                    await client.ConnectAsync(serverHost, PORT).AsTask(new CancellationTokenSource(1000).Token);
+                    client.Connect(ADDR, PORT);
+
                     Status = SerialPortStatus.Opened;
 
                     _readCancellationTokenSource = new CancellationTokenSource();
@@ -58,55 +59,14 @@ namespace Shunxi.Business.Protocols
 
         public async Task Listen()
         {
-            try
-            {
-                reader = new DataReader(client.InputStream);
-                reader.InputStreamOptions = InputStreamOptions.Partial;
-
-                while (!_readCancellationTokenSource.IsCancellationRequested)
-                {
-                    var len = await reader.LoadAsync(1024);  //获取一定大小的数据流  
-                    var xdata = new byte[len];
-                    reader.ReadBytes(xdata);
-                    reader.DetachBuffer();
-
-                    LogFactory.Create().Info($"receive ->{Common.BytesToString(xdata)}<- receive end");
-                    OnReceiveHandler(xdata);
-
-                    await Task.Delay(5, _readCancellationTokenSource.Token);
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                LogFactory.Create().Info("net serial listen cancel");
-                Status = SerialPortStatus.Initialled;
-            }
-            catch (Exception ex)
-            {
-                LogFactory.Create().Info("net serial listen error" + ex.Message);
-                Status = SerialPortStatus.Initialled;
-            }
-            finally
-            {
-                if (reader != null)
-                {
-                    reader.DetachBuffer();
-                    reader.DetachStream();
-                    reader.Dispose();
-                }
-            }
+            await Task.Yield();
         }
 
-        public async Task Send(byte[] data, CancellationToken token)
+        public void Send(byte[] data, CancellationToken token)
         {
             try
             {
-                DataWriter writer = new DataWriter(client.OutputStream);  
-                writer.WriteBytes(data);  
-                await writer.StoreAsync().AsTask(token);
-                LogFactory.Create().Info($"send ->{Common.BytesToString(data.ToArray())}<- send end");
-                writer.DetachStream();  
-                writer.Dispose();  
+                client.Send(data);
             }
             catch (Exception ex)
             {
@@ -132,7 +92,6 @@ namespace Shunxi.Business.Protocols
             try
             {
                 Cancel();
-                client?.Dispose();
                 Status = SerialPortStatus.None;
                 LogFactory.Create().Info("serialport dispose");
             }
@@ -145,6 +104,118 @@ namespace Shunxi.Business.Protocols
         protected virtual void OnReceiveHandler(byte[] obj)
         {
             ReceiveHandler?.Invoke(obj);
+        }
+    }
+
+
+    public class SocketClient
+    {
+        private Socket newclient; //= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly byte[] buffer = new byte[1024];
+
+        ~SocketClient()
+        {
+            Close();
+        }
+
+        public bool IsIP(string ip)
+        {
+            return Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+        }
+
+        public void ByteMencpy(byte[] bufferDart, byte[] bufferOrig, int lend)
+        {
+            for (int i = 0; i < lend; i++)
+            {
+                bufferDart[i] = bufferOrig[i];
+            }
+        }
+
+        public bool Connect(string ipString, int port)
+        {
+            var ie = IsIP(ipString) 
+                ? new IPEndPoint(IPAddress.Parse(ipString), port) 
+                : new IPEndPoint(Dns.GetHostEntry(ipString).AddressList[0], port);
+
+            try
+            {
+                newclient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                newclient.Connect(ie);
+                newclient.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), newclient);
+            }
+            catch (SocketException e)
+            {
+                LogFactory.Create().Error("unable to connect to server " + e.Message) ;
+                return false;
+            }
+
+            return true;
+        }
+
+        void ReceiveCallback(IAsyncResult result)
+        {
+            try
+            {
+                Socket ts = (Socket) result.AsyncState;
+                int len = ts.EndReceive(result);
+                result.AsyncWaitHandle.Close();
+                if (len > 0)
+                {
+                    byte[] bufferTemp = new byte[len];
+                    ByteMencpy(bufferTemp, buffer, len);
+                    LogFactory.Create().Info("receive ->" + Common.Utility.Common.BytesToString(bufferTemp) + "<- receive end");
+                }
+
+                ts.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), ts);
+            }
+            catch (SocketException e)
+            {
+                LogFactory.Create().Info("接收异常:" + e.Message);
+            }
+        }
+
+        public bool Send(string sendStr)
+        {
+            byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+            LogFactory.Create().Info("send ->" + Encoding.ASCII.GetString(bs) + "<- send end");
+            newclient.Send(bs);
+            return true;
+        }
+
+        public bool Send(byte[] bs)
+        {
+            int len = -1;
+            try
+            {
+                LogFactory.Create().Info("send ->" + Common.Utility.Common.BytesToString(bs) + "<- send end");
+                len = newclient.Send(bs);
+            }
+            catch (Exception)
+            {
+                //
+            }
+
+            return len > 0;
+        }
+
+        public int Receive(byte[] recvBytes)
+        {
+            var bytes = newclient.Receive(recvBytes, recvBytes.Length, 0);
+            return bytes;
+        }
+
+        public bool Close()
+        {
+            try
+            {
+                newclient.Shutdown(SocketShutdown.Both);
+                newclient.Close();
+            }
+            catch (Exception)
+            {
+                //
+            }
+            return true;
         }
     }
 }
